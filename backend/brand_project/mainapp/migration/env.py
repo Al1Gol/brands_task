d958f20feee93540e0450cd03596bce5f58387a5
Korepanov A <al1working@mail.ru>
2025-06-20 02:32:22 +0300

    Обновление миграции

--- backend/brand_project/mainapp/migration/env.py
+++ backend/brand_project/mainapp/migration/env.py
@@ -20,7 +20,6 @@ from alembic import context
 # access to the values within the .ini file in use.
 config = context.config
 config.set_main_option("sqlalchemy.url", DATABASE_URL)
-
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
@@ -31,6 +30,7 @@ if config.config_file_name is not None:
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 target_metadata = Base.metadata
+
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
