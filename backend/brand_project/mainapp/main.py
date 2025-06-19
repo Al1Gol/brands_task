64bfae1481b6c951081371e65a1302723bacd77c
Korepanov A <al1working@mail.ru>
2025-06-20 02:11:38 +0300

    Логирование

--- backend/brand_project/mainapp/main.py
+++ backend/brand_project/mainapp/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
 from mainapp.router import router
+from fastapi.logger import logger
+
 import os
+import logging
 
 app = FastAPI()
 
 app.include_router(router)
+
+
+gunicorn_logger = logging.getLogger('gunicorn.error')
+logger.handlers = gunicorn_logger.handlers
+if __name__ != "main":
+    logger.setLevel(gunicorn_logger.level)
+else:
+    logger.setLevel(logging.DEBUG)
