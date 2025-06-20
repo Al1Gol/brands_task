9799911599d6195558abd7a1089e09306e390c36
AKorepanov <al1working@mail.ru>
2025-06-20 03:46:32 +0300

    Осуществлена полная настройка докеризации проекта

--- backend/brand_project/mainapp/config.py
+++ backend/brand_project/mainapp/config.py
@@ -8,9 +8,7 @@ class Settings(BaseSettings):
     DB_NAME: str
     DB_USER: str
     DB_PASSWORD: str
-    model_config = SettingsConfigDict(
-        env_file=os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", ".env")
-    )
+    model_config = SettingsConfigDict(env_file='/app/backend/brand_project/.env')
 
 settings = Settings()
 
